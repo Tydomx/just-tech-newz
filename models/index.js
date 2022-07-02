896f61ec039e47806ab2ff2af442b7bdab867d2a
Michael <michaelthai16@gmail.com>
2022-07-02 10:28:02 -0500

    created structure for post model

--- models/index.js
+++ models/index.js
@@ -1,4 +1,5 @@
 // responsible for importing User model and exporting and object w/ it's property
 const User = require('./User');
+const Post = require('./Post');
 
-module.exports = { User };
+module.exports = { User, Post };
